531ecf44765a065e96fac78271e8dc9c743097c9
Vivek Poddar <vivekimsit@gmail.com>
2018-04-28 10:13:48 +0100

    Remove morgan

--- web/server.js
+++ web/server.js
@@ -5,19 +5,12 @@ const cors = require('cors');
 const express = require('express');
 const fs = require('fs');
 const http = require('http');
-const morgan = require('morgan');
 const path = require('path');
 
 const { errorHandler, logErrors } = require('./utils');
 const routes = require('./routes');
 
 const app = express();
-// create a write stream (in append mode)
-const accessLogStream = fs.createWriteStream(
-  path.join(__dirname, 'access.log'),
-  { flags: 'a' }
-);
-app.use(morgan('combined', { stream: accessLogStream }));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
